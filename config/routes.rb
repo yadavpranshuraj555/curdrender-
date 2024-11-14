2d5cba54050df0754850fafc0b74715a195fc953
yadavpranshuraj555 <yadav.pranshuraj555@gmail.com>
2024-11-14 16:18:43 +0530

    routes are set

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'home/index'
   # devise_for :admin_users, ActiveAdmin::Devise.config
   # devise_for :admin_users, ActiveAdmin::Devise.config
 
@@ -14,6 +15,9 @@ Rails.application.routes.draw do
   post 'password_resets', to: 'password_resets#create'
   post 'password_resets/verify', to: 'password_resets#verify_code'
   post 'password_resets/reset_password', to: 'password_resets#reset_password'
+
+  # Set a root route for the root path ("/")
+  root 'home#index' # Update 'home#index' to the controller#action you want to display
 
   # Add any other routes as needed
 end
